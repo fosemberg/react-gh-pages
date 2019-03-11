02bd8d5a529b5f6e15392f5ce74032012be5209b
fosemberg <fosemberg@gmaill.com>
2019-03-11 05:42:16 +0300

    Updates

---
+++ precache-manifest.775dbf55c8a8fd695907538e95eca404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e8d18587bee1ed6968f",
+    "url": "/react-gh-pages/static/css/main.fa2b32af.chunk.css"
+  },
+  {
+    "revision": "5e8d18587bee1ed6968f",
+    "url": "/react-gh-pages/static/js/main.7d9855e0.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react-gh-pages/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b321707b7816e7f97e8df5fca4db313a",
+    "url": "/react-gh-pages/index.html"
+  }
+];
